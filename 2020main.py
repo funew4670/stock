a95a547f978ebbe303f673664f6392cc639a98c5
funew4670 <funew4670@gmail.com>
2020-05-03 23:31:05 +0800

    Create 2020main.py
    for windows

---
+++ 2020main.py
@@ -0,0 +1,55 @@
+
+import pandas as pd
+import requests
+import os.path
+import time
+
+path = ""
+StockListPath = "allstocks.csv"
+url = 'http://www.tse.com.tw/exchangeReport/STOCK_DAY'
+stocknum = ''
+
+df = pd.read_csv(StockListPath,header=None)
+data = df[[0,1]]
+
+for index,row in data.iterrows():
+    stocknum=str(row[0])
+    if not os.path.exists(path + stocknum):
+        os.makedirs(path + stocknum)
+
+    startdate=(row[1])
+    if startdate < 19990101:
+        startdate = "19990101"
+    else:
+        startdate = str(startdate)
+    print (stocknum+ " " + startdate)
+    
+    month_year_iter(int(startdate[4:6]), int(startdate[0:4]), 4, 2020)
+
+
+def month_year_iter( start_month, start_year, end_month, end_year ):
+
+    for year in range(start_year,end_year+1):
+        month_start =1
+        month_end = 13
+        if year == end_year:
+            month_end = end_month+1
+        if year == start_year:
+            month_start = start_month
+        for month in range(month_start,month_end):
+            directory = path +stocknum+"\\"+stocknum+"_"+str(year)+str('{:02d}'.format(month))+".json"
+            try:
+                r = requests.get(url, {
+
+                    'response': 'json',
+                    'date': str(year)+str('{:02d}'.format(month))+'01',
+                    'stockNo': stocknum,
+
+                })
+                #r.encoding = 'utf8'
+                with open(directory, "w") as text_file:
+                    text_file.write(r.text.encode('utf-8'))
+                    print directory
+            except requests.exceptions.ConnectionError:
+                r.status_code = "Connection refused"
+            time.sleep(30)
